350311209ae2b97db6f6781c19b659ea7a44e099
HorizonCode <horizoncode88@gmail.com>
2025-10-11 23:33:18 +0200

    feat: implement sorting functionality for server list based on query parameters

--- apps/frontend/src/routes/api/servers/+server.ts
+++ apps/frontend/src/routes/api/servers/+server.ts
@@ -2,7 +2,20 @@ import { getAllServers } from '@osu-server-list/db/query';
 import { json } from '@sveltejs/kit';
 import type { RequestEvent } from './$types';
 
-export const GET = async (_req: RequestEvent) => {
-	const servers = await getAllServers();
+export const GET = async (req: RequestEvent) => {
+	const sort = req.url.searchParams.get('sort') ?? 'onlinePlayers';
+
+	let servers = await getAllServers();
+
+	if (sort === 'onlinePlayers') {
+		servers = servers.sort((a, b) => b.onlinePlayers - a.onlinePlayers);
+	} else if (sort === 'registeredPlayers') {
+		servers = servers.sort((a, b) => b.registeredPlayers - a.registeredPlayers);
+	} else if (sort === 'trending') {
+		servers = servers.sort((a, b) => b.trending - a.trending);
+	} else if(sort === "votes") {
+        servers = servers.sort((a, b) => b.votes - a.votes);
+    }
+
 	return json(servers);
 };
