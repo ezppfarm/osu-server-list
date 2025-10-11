c36f027872982693b20758304f4d301166ec8150
HorizonCode <horizoncode88@gmail.com>
2025-10-11 23:32:19 +0200

    feat: enhance getAllServers query to include detailed server statistics and join with status and vote tables

--- packages/db/src/query.ts
+++ packages/db/src/query.ts
@@ -1,4 +1,29 @@
+import { asc, desc, eq, sql } from "drizzle-orm";
 import { db } from ".";
-import { server } from "./schema";
+import { server, serverStatus, serverVote } from "./schema";
+import { intWithFallback, sumAsIntWithFallback } from "./util";
+import { int } from "drizzle-orm/mysql-core";
 
-export const getAllServers = async () => await db.select().from(server);
+export const getAllServers = async () =>
+  await db
+    .select({
+      id: server.id,
+      name: server.name,
+      description: server.description,
+      url: server.url,
+      iconUrl: server.iconUrl,
+      tags: server.tags,
+      trending: server.trending,
+      onlinePlayers: intWithFallback(serverStatus.onlinePlayers, -1).as(
+        "onlinePlayers"
+      ),
+      registeredPlayers: intWithFallback(serverStatus.registeredPlayers, -1).as(
+        "registeredPlayers"
+      ),
+      votes: sumAsIntWithFallback(serverVote.id, 0).as("votes"),
+    })
+    .from(server)
+    .leftJoin(serverStatus, eq(server.id, serverStatus.serverId))
+    .leftJoin(serverVote, eq(server.id, serverVote.serverId))
+    .groupBy(server.id)
+    .orderBy(asc(server.id), desc(serverStatus.timestamp));
--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,4 +1,4 @@
-import { mysqlTable, int, text } from "drizzle-orm/mysql-core";
+import { mysqlTable, int, text, datetime } from "drizzle-orm/mysql-core";
 
 export const user = mysqlTable("user", {
   id: int().primaryKey().autoincrement().notNull(),
@@ -8,10 +8,33 @@ export const user = mysqlTable("user", {
 
 export const server = mysqlTable("server", {
   id: int().primaryKey().autoincrement().notNull(),
+  type: text({ enum: ["BANCHOPY", "RIPPLE", "CUSTOM"] })
+    .default("BANCHOPY")
+    .notNull(),
   name: text().notNull(),
   description: text(),
   url: text().notNull(),
   iconUrl: text().notNull(),
   tags: text(),
   trending: int().notNull(),
+});
+
+export const serverStatus = mysqlTable("server_status", {
+  id: int().primaryKey().autoincrement().notNull(),
+  serverId: int()
+    .references(() => server.id)
+    .notNull(),
+  timestamp: datetime().default(new Date()).notNull(),
+  onlinePlayers: int().notNull(),
+  registeredPlayers: int().notNull(),
+});
+
+export const serverVote = mysqlTable("server_vote", {
+  id: int().primaryKey().autoincrement().notNull(),
+  serverId: int()
+    .references(() => server.id)
+    .notNull(),
+  username: text().notNull(),
+  ip: text().notNull(),
+  timestamp: datetime().default(new Date()).notNull(),
 });
