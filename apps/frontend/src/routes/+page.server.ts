b28067b7fd41ac36f5aebd6111b9cdadd64f2d27
HorizonCode <horizoncode88@gmail.com>
2025-10-12 20:39:19 +0200

    fix: sort users by online players for now

--- apps/frontend/src/routes/+page.server.ts
+++ apps/frontend/src/routes/+page.server.ts
@@ -3,7 +3,9 @@ import { getAllServers } from '@osu-server-list/db/query';
 export async function load() {
 	const servers = await getAllServers();
 
+
+
 	return {
-		servers
+		servers: servers.sort((a, b) => b.onlinePlayers - a.onlinePlayers)
 	};
 }
