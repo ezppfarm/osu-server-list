d9145cfe8f0bdcdd1c4daba0c6d88f0c17ec9d9c
HorizonCode <horizoncode88@gmail.com>
2025-10-12 14:36:48 +0200

    feat: implement server status job to fetch and store online player counts

--- apps/crawler/src/index.ts
+++ apps/crawler/src/index.ts
@@ -1,1 +1,50 @@
-console.log("Hello via Bun!");
+import { betterFetch } from "@better-fetch/fetch";
+import { addServerStatus, getAllServers } from "@osu-server-list/db/query";
+import Baker from "cronbake";
+import type { BpyUsersResponse } from "./types";
+
+const baker = Baker.create();
+
+baker.add({
+  name: "server-status-job",
+  cron: "@every_minute",
+  overrunProtection: true,
+  callback: async () => {
+    const servers = await getAllServers();
+
+    for (const server of servers) {
+      let onlinePlayers = -1;
+      let registeredPlayers = -1;
+      let ping = -1;
+
+      let pingStart = Bun.nanoseconds();
+      try {
+        if (server.type === "BANCHOPY") {
+          const serverApiUrl = server.url.replace("https://", "https://api.");
+          const serverRequest = await betterFetch<BpyUsersResponse>(
+            serverApiUrl + "/v1/get_player_count"
+          );
+          if (!serverRequest.error) {
+            onlinePlayers = serverRequest.data.counts.online;
+            registeredPlayers = serverRequest.data.counts.total;
+          }
+        }
+      } catch (e) {
+        console.log("Error fetching server status:");
+        console.log(e);
+      }
+
+      if (onlinePlayers > -1 && registeredPlayers > -1)
+        ping = Number(((Bun.nanoseconds() - pingStart) / 1000000).toFixed());
+
+      await addServerStatus(server.id, onlinePlayers, registeredPlayers, ping);
+    }
+  },
+});
+
+baker.bakeAll();
+console.log("CronJob started.");
+process.on("SIGINT", () => {
+  baker.destroyAll();
+  console.log("CronJob stopped.");
+});
---
+++ apps/crawler/src/types.ts
@@ -0,0 +1,7 @@
+export type BpyUsersResponse = {
+  status: string;
+  counts: {
+    online: number;
+    total: number;
+  };
+};
