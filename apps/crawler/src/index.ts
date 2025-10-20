4a4e400e29fccd8dc2c6dde08c48e83285909d17
HorizonCode <horizoncode88@gmail.com>
2025-10-20 22:42:47 +0200

    fix(crawler): handle errors in server status fetching more gracefully

--- apps/crawler/src/index.ts
+++ apps/crawler/src/index.ts
@@ -11,35 +11,40 @@ baker.add({
   start: true,
   overrunProtection: true,
   callback: async () => {
-    const servers = await getAllServersRaw();
+    try {
+      const servers = await getAllServersRaw();
 
-    for (const server of servers) {
-      console.log(`Fetching status for ${server.name}...`);
+      for (const server of servers) {
+        console.log(`Fetching status for ${server.name}...`);
 
-      let counts: UsersResponse = { onlineCount: -1, totalCount: -1 };
-      let ping = -1;
+        let counts: UsersResponse = { onlineCount: -1, totalCount: -1 };
+        let ping = -1;
 
-      let pingStart = Bun.nanoseconds();
-      try {
-        const apiHandler = getApiHandler(server.url, server.type);
-        counts = await apiHandler.fetchUserCounts();
-      } catch (e) {
-        console.log("Error fetching server status:");
-        console.log(e);
+        let pingStart = Bun.nanoseconds();
+        try {
+          const apiHandler = getApiHandler(server.url, server.type);
+          counts = await apiHandler.fetchUserCounts();
+        } catch (e) {
+          console.log("Error fetching server status:");
+          console.log(e);
+        }
+
+        if (counts.onlineCount > -1)
+          ping = Number(((Bun.nanoseconds() - pingStart) / 1000000).toFixed());
+
+        await addServerStatus(
+          server.id,
+          counts.onlineCount,
+          counts.totalCount,
+          ping,
+        );
+        console.log(
+          `Fetched status for ${server.name}: ${counts.onlineCount} online, ${counts.totalCount} total, ${ping}ms ping`,
+        );
       }
-
-      if (counts.onlineCount > -1)
-        ping = Number(((Bun.nanoseconds() - pingStart) / 1000000).toFixed());
-
-      await addServerStatus(
-        server.id,
-        counts.onlineCount,
-        counts.totalCount,
-        ping,
-      );
-      console.log(
-        `Fetched status for ${server.name}: ${counts.onlineCount} online, ${counts.totalCount} total, ${ping}ms ping`,
-      );
+    } catch (e) {
+      console.log("Error in server-status-job:");
+      console.log(e);
     }
   },
 });
