56c9c1837bb244599192ee7c94879a3fc7eb349e
HorizonCode <horizoncode88@gmail.com>
2025-10-26 22:32:20 +0100

    fix(frontend): change server icon display style to object-contain

--- apps/frontend/src/routes/server/[id]/+page.svelte
+++ apps/frontend/src/routes/server/[id]/+page.svelte
@@ -143,7 +143,7 @@
 								<div
 									class="flex h-32 w-32 items-center justify-center overflow-hidden rounded-2xl border bg-primary/10 p-1"
 								>
-									<img src={server.iconUrl} alt={server.name} class="h-full w-full object-cover" />
+									<img src={server.iconUrl} alt={server.name} class="h-full w-full object-contain" />
 								</div>
 							</div>
 
