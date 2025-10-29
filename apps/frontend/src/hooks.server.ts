af1ae192fcb527c9ea01861aed75b49ec25685e5
HorizonCode <horizoncode88@gmail.com>
2025-10-29 16:24:31 +0100

    chore: remove username and password login for now

--- apps/frontend/src/hooks.server.ts
+++ apps/frontend/src/hooks.server.ts
@@ -1,32 +1,5 @@
-import { decrypt } from '@/crypto';
-import { getUser } from '@osu-server-list/db/query';
-import type { Handle, RequestEvent } from '@sveltejs/kit';
-
-const auth = async (event: RequestEvent) => {
-	const encryptedSession = event.cookies.get('session') ?? '';
-	if (encryptedSession) {
-		const decryptedSession = decrypt(encryptedSession);
-		const [, name, passwordHash] = decryptedSession.split(':');
-		const user = await getUser(name, passwordHash);
-		if (user) {
-			event.locals.user = {
-				id: user.id,
-				name: user.name
-			};
-			event.cookies.set('session', encryptedSession, {
-				httpOnly: true,
-				secure: true,
-				sameSite: 'lax',
-				path: '/',
-				maxAge: 3600 // 1 hour expiration
-			});
-		} else {
-			event.cookies.delete('session', { path: '/' });
-		}
-	}
-};
+import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	await auth(event);
 	return await resolve(event);
 };
