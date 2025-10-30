399e42a7873ed24e00275a106da32d0195304a1c
HorizonCode <horizoncode88@gmail.com>
2025-10-30 14:58:33 +0100

    fix: deny access if user is no system admin

--- apps/frontend/src/routes/admin/+layout.server.ts
+++ apps/frontend/src/routes/admin/+layout.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
 
 export const load = (req) => {
-	if (!req.locals.session || !req.locals.session.manage.manageServers) return redirect(302, '/');
+	if (!req.locals.session || !req.locals.session.manage.systemAdmin) return redirect(302, '/');
 };
