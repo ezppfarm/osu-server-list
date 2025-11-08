6dc647b29f2eb4fcc574b8538d44f49aea500abb
HorizonCode <horizoncode88@gmail.com>
2025-11-08 10:41:36 +0100

    chore: extend meta tags

--- apps/frontend/src/routes/+layout.server.ts
+++ apps/frontend/src/routes/+layout.server.ts
@@ -6,6 +6,34 @@ export const ssr = true;
 
 export const load = (req: RequestEvent) => {
 	const baseMetaTags = Object.freeze({
+		robots: true,
+		title: `${env.PUBLIC_APP_NAME} - Your Gateway to osu! Communities`,
+		description:
+			'Browse, compare, and join thriving osu! private servers. Find communities with custom features, unique gameplay modes, and active player bases.',
+		keywords: [
+			'osu',
+			'osu!',
+			'private server',
+			'osu private servers',
+			'osu! private servers',
+			'osu communities',
+			'osu! communities',
+			'osu server list',
+			'osu! server list',
+			'private servers',
+			'rhythm game',
+			'community',
+			'multiplayer',
+			'custom features',
+			'gameplay modes'
+		],
+		twitter: {
+			title: `${env.PUBLIC_APP_NAME} - Your Gateway to osu! Communities`,
+			image: `${req.url.protocol}//${req.url.host}/logo.png`,
+			description:
+				'Browse, compare, and join thriving osu! private servers. Find communities with custom features, unique gameplay modes, and active player bases.'
+		},
+		canonical: req.url.href,
 		openGraph: {
 			type: 'website',
 			url: req.url.href,
