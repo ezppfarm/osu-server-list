0d25b25c3c87d4a4304ff2d3242d2897be47fd65
Aochi <me@aochi.uk>
2026-04-09 19:12:28 +0200

    oops.

--- packages/db/drizzle/0007_heavy_skin.sql
+++ packages/db/drizzle/0007_heavy_skin.sql
@@ -1,4 +1,4 @@
+CREATE INDEX `server_timestamp_ping_idx` ON `server_status` (`serverId`,`timestamp`,`ping`);--> statement-breakpoint
 DROP INDEX `server_timestamp_idx` ON `server_status`;--> statement-breakpoint
 DROP INDEX `server_idx` ON `server_vote`;--> statement-breakpoint
-CREATE INDEX `server_timestamp_ping_idx` ON `server_status` (`serverId`,`timestamp`,`ping`);--> statement-breakpoint
 CREATE INDEX `server_timestamp_idx` ON `server_vote` (`serverId`,`timestamp`);
