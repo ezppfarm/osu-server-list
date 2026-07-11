2103a2d76acf703f79dee6935993c45d58206f11
Aochi <me@aochi.uk>
2026-07-12 01:20:44 +0200

    add server request schema

--- packages/db/src/types.ts
+++ packages/db/src/types.ts
@@ -55,3 +55,35 @@ export type ServerManage = {
   systemAdmin: boolean;
   manageServers: Server[];
 };
+
+export type ServerRequestStatus = "PENDING" | "ACCEPTED" | "DENIED";
+
+export type ServerRequest = {
+  id: number;
+  discordId: string;
+  status: ServerRequestStatus;
+  type: "BANCHOPY" | "RIPPLE" | "TITANIC" | "SUNRISE" | "CUSTOM";
+  name: string;
+  description: string | null;
+  url: string;
+  iconUrl: string;
+  discordUrl: string | null;
+  tags: string | null;
+  location: string | null;
+  denialReason: string | null;
+  createdServerId: number | null;
+  submittedAt: number;
+  reviewedAt: number | null;
+  seen: number;
+};
+
+export type ServerRequestInput = {
+  name: string;
+  type: "BANCHOPY" | "RIPPLE" | "TITANIC" | "SUNRISE" | "CUSTOM";
+  description: string;
+  url: string;
+  iconUrl: string;
+  discordUrl: string;
+  tags: string;
+  location: string;
+};
