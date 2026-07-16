cea60512c5c059b68f6a05139ea181c19823e855
HorizonCode <horizoncode88@gmail.com>
2026-07-16 09:37:54 +0200

    chore: use tldts to parse correct domain names

--- packages/handlers/src/utils.ts
+++ packages/handlers/src/utils.ts
@@ -1,12 +1,15 @@
+import { getDomain } from "tldts";
+
 export function rewriteUrl(input: string, subdomain: string): string {
   const url = new URL(input);
 
-  const parts = url.hostname.split(".");
+  const domain = getDomain(url.hostname);
 
-  // Keep the root domain
-  const root = parts.slice(-2).join(".");
+  if (!domain) {
+    throw new Error(`Unable to determine registrable domain for "${url.hostname}"`);
+  }
 
-  url.hostname = `${subdomain}${root}`;
+  url.hostname = `${subdomain}${domain}`;
 
   return url.toString();
 }
